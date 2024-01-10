64c73e13f3173d31568c76de5b52c28e0f0f5dbe
Nikhil Goli <ngoli@umich.edu>
2024-01-10 13:17:30 -0500

    The bot is complete

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ welcome_msg += "I am a bot creates AI generated stories in response to writing p
 print(welcome_msg)
 
 #asks user to generate story
+user = input("Would you like to generate a story?\n")
 while True:
-    user = input("Would you like to generate a story?\n")
     if user in yesresponse:
-        prompt = find_prompt()
-        user = input("Would you like to generate another story?\n")
+        prompt = make_post()
+        user = input("\nWould you like to generate another story?\n")
     elif user in noresponse:
         print("\nThank you for using the Reddit Story Bot!\n")
         break
--- reddit_bot.py
+++ reddit_bot.py
@@ -26,34 +26,51 @@ def format_prompt(submission):
 
 #saves post id and replies to post with the gpt generated story
 def post_story(submission, story):
-    posts_replied_to = load_posts_replied_to()
-    posts_replied_to.append(submission.id)
-    with open("posts_replied_to.txt", "w") as f:
-        for submission_id in posts_replied_to:
-            f.write(submission_id + "\n")
-    submission.reply(story)
+    user = input("\nWould you like to post this story on the r/WritingPrompts subreddit?\n")
+    while True:
+        if user in yesresponse:
+            print("\nAwesome! Please wait while I post your story...\n")
+            posts_replied_to = load_posts_replied_to()
+            posts_replied_to.append(submission.id)
+            with open("posts_replied_to.txt", "w") as f:
+                for submission_id in posts_replied_to:
+                    f.write(submission_id + "\n")
+            submission.reply(story)
+            print("Your story has been successfully posted.\nYou can view it here:\n\nhttps://www.reddit.com" + submission.permalink + "\n")
+            return
+        elif user in noresponse:
+            print("Sorry to hear that. I hope you will post the next story you generate!")
+            return
+        else:
+            user = input("I didn't understand that. Please answer 'yes' or 'no'.\n")
+        
+#Asks if the user is satisifed with the prompt found by the bot
+def approve_prompt(prompt):
+    print("\n" + prompt + "\n")
+    while True:
+        user_approval = input("Is this prompt good?\n")
+        if user_approval in yesresponse:
+            print("\nGreat!\n")
+            return True
+        elif user_approval in noresponse:
+            print("\nNo worries, I will find another prompt.\n")
+            return False
+        else:
+            print("\nPlease answer 'yes' or 'no'.\n")
 
 #searches for a satisfactory prompt for the user
-def find_prompt():
-    print("Fantastic! Please wait while I find a prompt.\n")
+def make_post():
+    print("\nFantastic! Please wait while I find a prompt.\n")
     subreddit = reddit.subreddit('WritingPrompts')
-
     for submission in subreddit.hot():
         if check_valid_prompt(submission):
             prompt = format_prompt(submission)
-            print("\n" + prompt + "\n")
-
-            while True:
-                user_approval = input("Is this prompt good?\n")
-                if user_approval in yesresponse:
-                    print("\nGreat!\n")
-                    return prompt
-                elif user_approval in noresponse:
-                    print("\nNo worries, I will find another prompt.\n")
-                    break
-                else:
-                    print("\nPlease answer 'yes' or 'no'.\n")
-
+            if approve_prompt(prompt):
+                print("Please wait while I generate your story...\n")
+                story = generate_story(prompt)
+                print("Here is your story:\n\n" + story)
+                post_story(submission, story)
+                return
 
 #locate and load environment variables from .env file
 dotenv_path = find_dotenv()
@@ -75,17 +92,3 @@ reddit = praw.Reddit(client_id = client_id,
 
 yesresponse = {"yes", "Yes", "YES", "y", "Y"}
 noresponse = {"no", "No", "NO", "n", "N"}
-
-#make the post
-'''
-subreddit = reddit.subreddit('WritingPrompts')
-posts_made = 0
-for submission in subreddit.hot():
-    if posts_made < 1 and check_valid_prompt(submission):
-        prompt = format_prompt(submission)
-        story = generate_story(prompt)
-        post_story(submission, story)
-        posts_made += 1
-'''
-#allow user to choose length of story and number of stories made
-#the stories are currently just longer versions of the prompt, need to find a way to make them more thrilling and not just expansions of the prompt
